27dc251282752c415525243118ca5470160c3a9b
Matias Vinjevoll <matias.vinjevoll@nav.no>
2024-09-17 09:27:47 +0200

    Legg til routes for health

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import express from "express";
-import routes from "./routes.js";
+import apiRoutes from "./apiRoutes.js";
 import { validateToken } from "./tokenValidation.js";
+import internalRoutes from "./internalRoutes.js";
 
 const app = express();
-
 const port = Number(process.env.PORT) || 8080;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/internal", internalRoutes);
+
 app.use(validateToken);
-app.use("/api", routes);
+
+app.use("/api", apiRoutes);
 
 app.listen(port, () => {
   console.log(`Server now listening on port: ${port}`);
