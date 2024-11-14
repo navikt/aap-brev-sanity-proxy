2242cd0c2fe52e54f8166a8614af7d552487eecf
Matias Vinjevoll <matias.vinjevoll@nav.no>
2024-11-14 12:55:44 +0100

    Endre port
    Co-authored-by: Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import "dotenv/config";
-import express from "express";
-import apiRoutes from "./apiRoutes.js";
-import { validateToken } from "./tokenValidation.js";
-import internalRoutes from "./internalRoutes.js";
+import 'dotenv/config';
+import express from 'express';
+import apiRoutes from './apiRoutes.js';
+import { validateToken } from './tokenValidation.js';
+import internalRoutes from './internalRoutes.js';
 
 const app = express();
-const port = Number(process.env.PORT) || 8080;
+const port = Number(process.env.PORT) || 8087;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/internal", internalRoutes);
+app.use('/internal', internalRoutes);
 
 app.use(validateToken);
 
-app.use("/api", apiRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(port, () => {
   console.log(`Server now listening on port: ${port}`);
