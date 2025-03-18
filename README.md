113414a533072a324a55b11c1c96ad27c946bca3
Fredrik Meyer <hrmeyer@gmail.com>
2025-03-18 14:13:46 +0100

    Bruk bundle (#24)
    * Bruk bundle
    
    * Legg til fremgangsmåte for lokal testing
    
    ---------
    
    Co-authored-by: Matias Vinjevoll <matias.vinjevoll@nav.no>

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # aap-brev-sanity-proxy
+
+## Hvordan teste lokalt
+
+Lag en `.env` fil og legg inn følgende
+```
+SANITY_DATASET="production"
+SANITY_PROJECT_ID="948n95rd"
+
+SANITY_API_READ_TOKEN="<ditt token her>"
+```
+
+### Testing av bygg lokalt
+
+`yarn install && yarn build`
+
+`docker build -t aap-brev-sanity-proxy-local-build .`
+
+kopier verdi for `SANITY_API_READ_TOKEN` til clipboard
+
+```
+docker run -d --rm -p 8087:8087 \
+    -e "SANITY_API_READ_TOKEN=`pbpaste`" \
+    -e "SANITY_PROJECT_ID=948n95rd" \
+    -e "SANITY_DATASET=production" \
+    -e "NODE_ENV=development" \
+    aap-brev-sanity-proxy-local-build:latest
+```
--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,19 +1,12 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-02-27";
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-02-27';
 
-export const dataset = assertValue(
-  process.env.SANITY_DATASET,
-  "Missing environment variable: SANITY_DATASET",
-);
+export const dataset = assertValue(process.env.SANITY_DATASET, 'Missing environment variable: SANITY_DATASET');
 
-export const projectId = assertValue(
-  process.env.SANITY_PROJECT_ID,
-  "Missing environment variable: SANITY_PROJECT_ID",
-);
+export const projectId = assertValue(process.env.SANITY_PROJECT_ID, 'Missing environment variable: SANITY_PROJECT_ID');
 
 export const sanityReadToken = assertValue(
   process.env.SANITY_API_READ_TOKEN,
-  "Missing environment variable: b",
+  'Missing environment variable: SANITY_API_READ_TOKEN'
 );
 
 export const useCdn = false;
