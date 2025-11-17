5173970daf77933c1b44475e1e0d27e5c73ff32c
Matias Vinjevoll <1862220+matiasvinjevoll@users.noreply.github.com>
2025-11-17 13:34:47 +0100

    API-endepunkt for å hente ut brevmal for ny modell (#108)
    * API-endepunkt for å hente ut brevmal for ny modell
    
    * legg til qroq

--- src/apiRoutes.ts
+++ src/apiRoutes.ts
@@ -4,6 +4,7 @@ import { Brevtype } from './brevtype.js';
 import { flettBrev } from './brevService.js';
 import { brevmalToPdf } from './pdfService';
 import { GenererPdfRequest } from './pdfModell';
+import { hentBrevmal } from './brevmalService';
 
 const router = express.Router();
 
@@ -21,6 +22,26 @@ router.get('/mal', async (req, res, next) => {
   try {
     const flettetBrev = await flettBrev(brevtype, språk);
     res.send(flettetBrev);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Ny brevmodell
+router.get('/brevmal', async (req, res, next) => {
+  const brevtype = Object.values(Brevtype).find((x) => x === req.query['brevtype']);
+  if (!brevtype) {
+    return res.status(400).send('Mangler brevtype');
+  }
+
+  const språk = Object.values(Språk).find((x) => x === req.query['sprak']);
+  if (!språk) {
+    return res.status(400).send('Mangler språk');
+  }
+
+  try {
+    const brevmal = await hentBrevmal(brevtype, språk);
+    res.send(brevmal);
   } catch (err) {
     next(err);
   }
---
+++ src/brevmalService.ts
@@ -0,0 +1,22 @@
+import { Språk } from './språk.js';
+import { Brevtype } from './brevtype.js';
+import { client } from './sanity/client';
+import { MalQuery } from './sanity/brevmalQuery';
+
+// Ny brevmodell
+export async function hentBrevmal(brevtype: Brevtype, språk: Språk) {
+  const malId = brevtypeTilSanityId(brevtype);
+
+  return client.fetch(MalQuery, { id: malId, lang: språk });
+}
+
+const brevtypeTilSanityId = (brevtype: Brevtype) => {
+  switch (brevtype) {
+    case Brevtype.INNVILGELSE:
+      return 'f0af3bf5-0a3c-47a2-affb-94b2b544c710';
+    case Brevtype.AVSLAG:
+      return '40a9777b-b6fc-43b1-aec9-aa5d3357db7a';
+    default:
+      throw new Error(`Mangler mapping fra brevtype ${brevtype} til id`);
+  }
+};
