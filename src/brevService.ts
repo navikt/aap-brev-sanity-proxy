85303b8cb3a18e773b2164b78494f3f979e6ed83
Greger Siem Knudsen <greger.siem.knudsen@nav.no>
2025-11-21 14:42:38 +0100

    La til riktig id for brevtype 11-23 sjette ledd

--- src/brevService.ts
+++ src/brevService.ts
@@ -22,7 +22,7 @@ const brevtypeTilSanityId = {
   [Brevtype.INNVILGELSE]: '37594d86-38d4-4f2a-9b75-cdc0cbe02e08',
   [Brevtype.VEDTAK_11_17]: '7365300c-6651-4b92-a2d9-6accd8b89b16',
   [Brevtype.VEDTAK_11_18]: 'd27e357b-bc38-4c8c-8ae5-4e398fc854a3',
-  [Brevtype.VEDTAK_11_23_SJETTE_LEDD]: '93e6b517-187a-4e73-abba-1fe99c329065',
+  [Brevtype.VEDTAK_11_23_SJETTE_LEDD]: '2f904e2e-abde-4bda-9613-851845be2dc9',
   [Brevtype.AVSLAG]: '540d061b-0772-4e78-b078-40483c0192f2',
   [Brevtype.VARSEL_OM_BESTILLING]: 'c16dc9a8-d41c-4a73-bc79-af95902d40c3',
   [Brevtype.FORHÅNDSVARSEL_BRUDD_AKTIVITETSPLIKT]: '1da6c37e-f9d4-46ca-8bc0-81430c91b84d',
