66c5171b1bb7602097ac7b01fd0d440aade0814f
Øivind Stensrud <oivind.stensrud@nav.no>
2025-12-08 16:26:20 +0100

    Oppdatert id for innvilgelse og ny id for 11-18

--- src/brevmalService.ts
+++ src/brevmalService.ts
@@ -12,8 +12,10 @@ export async function hentBrevmal(brevtype: Brevtype, språk: Språk) {
 
 const brevtypeTilSanityId = (brevtype: Brevtype) => {
   switch (brevtype) {
-    case Brevtype.INNVILGELSE:
+    case Brevtype.VEDTAK_11_18:
       return 'f0af3bf5-0a3c-47a2-affb-94b2b544c710';
+    case Brevtype.INNVILGELSE:
+      return '6b229785-4514-4506-8988-8ac81af51503';
     case Brevtype.AVSLAG:
       return '40a9777b-b6fc-43b1-aec9-aa5d3357db7a';
     default:
