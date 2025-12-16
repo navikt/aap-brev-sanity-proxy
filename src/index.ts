63beb2369a875b9dedd08617b0b8b5f26df9bee4
Øivind Stensrud <oivind.stensrud@nav.no>
2025-12-16 10:40:04 +0100

    Øker request-størrelse

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 const port = Number(process.env.PORT) || 8087;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: '200kb' }));
 
 app.use('/internal', internalRoutes);
 
