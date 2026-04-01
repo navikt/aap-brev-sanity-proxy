0f9687fa73c7a2a8efafc0c8049c9a4c6dcf192a
Matias Vinjevoll <matias.vinjevoll@nav.no>
2026-04-01 08:38:50 +0200

    Øker begrensning request-størrelse ytterligere

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 const port = Number(process.env.PORT) || 8087;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: '200kb' }));
+app.use(express.json({ limit: '300kb' }));
 
 app.use('/internal', internalRoutes);
 
