bce1eecaee16a01ba4e691c782d18f72de03ada0
Tor Idland <tor.idland@nav.no>
2026-09-10 10:15:33 +0200

    logg brevhtml i dev

--- src/envUtils.ts
+++ src/envUtils.ts
@@ -1,3 +1,7 @@
+const DEV_CLUSTER = 'dev-gcp';
+
+export const isDevGcp = (): boolean => process.env.NAIS_CLUSTER_NAME === DEV_CLUSTER;
+
 export function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage);
