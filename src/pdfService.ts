2a967b90ac7468c875ca1e3f864b4f84de8bc8a8
VHollund <vetle.hollund@nav.no>
2026-09-17 10:05:54 +0200

    Revert "AAP-BAU - Legg til unleash med test toggle (#325)"
    This reverts commit 58fd36e82bca3d2af9bb68c11bd257979e2d3ee0.

--- src/pdfService.ts
+++ src/pdfService.ts
@@ -2,14 +2,10 @@ import { genererHtml } from './components/GenererHtml';
 import { assertValue, isDevGcp } from './envUtils';
 import { GenererPdfRequest } from './pdfModell';
 import { logger } from '@navikt/pino-logger';
-import { unleash } from './unleash';
 
 const pdfgenUrl = assertValue(process.env.PDF_GEN_URL, 'Missing environment variable: PDF_GEN_URL');
 
 export async function brevmalToPdf(request: GenererPdfRequest) {
-  if(unleash.isEnabled('BrevSanityProxyTest')) {
-    logger.info('BrevSanityProxyTest enabled' );
-  }
   const html = genererHtml(request);
   if(isDevGcp()){
     logger.info(html);
