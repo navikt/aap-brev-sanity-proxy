ab100ebf63d7329700d0896161eacfa9bb989811
Tor Idland <tor.idland@nav.no>
2026-09-23 09:41:49 +0200

    AAP-BAU debug log i dev

--- src/pdfService.ts
+++ src/pdfService.ts
@@ -1,7 +1,8 @@
 
 import { genererHtml } from './components/GenererHtml';
-import { assertValue } from './envUtils';
+import { assertValue, isDevGcp } from './envUtils';
 import { GenererPdfRequest } from './pdfModell';
+import { logger } from '@navikt/pino-logger';
 
 const pdfgenUrl = assertValue(process.env.PDF_GEN_URL, 'Missing environment variable: PDF_GEN_URL');
 const pdfgeneratorUrl = assertValue(process.env.PDFGENERATOR_SAKSBEHANDLING_URL, 'Missing environment variable: PDFGENERATOR_SAKSBEHANDLING_URL');
@@ -22,6 +23,10 @@ export async function brevmalToPdf(request: GenererPdfRequest) {
 }
 export async function brevmalToPdfNyPdfgenerator(request: GenererPdfRequest) {
   const html = genererHtml(request);
+
+  if(isDevGcp()){
+    logger.info(html);
+  }
 
   const pdf = await fetch(pdfgeneratorUrl + '/api/v1/genpdf/html/saksbehandling', {
     method: 'POST',
